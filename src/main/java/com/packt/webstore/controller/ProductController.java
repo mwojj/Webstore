68e860c47311cc6025fb3b61e27f57c5302d5b7a
mwojj <30746796+mwojj@users.noreply.github.com>
2017-08-17 08:24:17 +0200

    Added sorting by filters

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.packt.webstore.domain.Product;
 import com.packt.webstore.domain.repository.ProductRepository;
 import com.packt.webstore.service.ProductService;
@@ -39,6 +41,25 @@ public class ProductController {
 	public String getProductsByFilter(Model model,
 			@MatrixVariable(pathVar = "ByCriteria") Map<String, List<String>> filterParams) {
 		model.addAttribute("products", productService.getProductsByFilter(filterParams));
+		return "products";
+	}
+
+	@RequestMapping("/products/filter/{prize}")
+	public String getProductsByPrize(Model model,
+			@MatrixVariable(pathVar = "prize") Map<String, List<String>> filterParams) {
+		model.addAttribute("products", productService.getProductsByFilter(filterParams));
+		return "products";
+	}
+
+	@RequestMapping("/products/product")
+	public String getProductById(Model model, @RequestParam("id") String productId) {
+		model.addAttribute("product", productService.getProductById(productId));
+		return "product";
+	}
+
+	@RequestMapping("/products/product-by-man")
+	public String getProductsByManufacturer(Model model, @RequestParam("manufacturer") String productManufacturer) {
+		model.addAttribute("products", productService.getProductsByManufacturer(productManufacturer));
 		return "products";
 	}
 
